1012ea440dc6a244b9aaac0cbaaf44ac8a942406
Priyasaha7 <148933801+Priyasaha7@users.noreply.github.com>
2026-02-01 13:50:44 +0530

    fix(chat): refetch messages on target user change and add error handling

--- src/Components/Chat.jsx
+++ src/Components/Chat.jsx
@@ -2,21 +2,47 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { createSocketConnection } from "../utils/socket";
 import { useSelector } from "react-redux";
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
 
 const Chat = () => {
   const { targetUserId } = useParams();
   const user = useSelector((store) => store.user);
   const userId = user?._id;
   const firstName = user?.firstName;
+  const lastName = user?.lastName;
 
   const [messages, setMessages] = useState([]);
   const [newMessage, setNewMessage] = useState("");
+
+  const fetchChatMesage = async () => {
+    const chat = await axios.get(BASE_URL + "/chat/" + targetUserId, {
+      withCredentials: true,
+    });
+    console.log(chat.data.messages);
+
+    const chatMessage = chat?.data?.messages.map((msg) => {
+      const { senderId, text } = msg;
+
+      return {
+        firstName: senderId?.firstName,
+        lastName: senderId?.lastName,
+        text,
+      };
+    });
+    setMessages(chatMessage);
+  };
+
+  useEffect(() => {
+    fetchChatMesage();
+  }, []);
 
   const sendMessage = () => {
     const socket = createSocketConnection();
 
     socket.emit("sendMessage", {
       firstName,
+      lastName,
       userId,
       targetUserId,
       text: newMessage,
@@ -54,9 +80,15 @@ const Chat = () => {
       {/* Messages */}
       <div className="flex-1 overflow-y-auto p-5 space-y-4">
         {messages.map((msg, index) => (
-          <div key={index} className="chat chat-start">
+          <div
+            key={index}
+            className={
+              "chat " +
+              (firstName === msg.firstName ? "chat-end" : "chat-start")
+            }
+          >
             <div className="chat-header">
-              {msg.firstName}
+              {msg.firstName} {msg.lastName}
               <time className="text-xs opacity-50 ml-2">1 sec ago</time>
             </div>
             <div className="chat-bubble">{msg.text}</div>
